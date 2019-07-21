a7e9fb8064f49fb3acc858c49d1cdc87b0d3deed
Nikolai Mushegian <hi@nikolai.fyi>
2019-07-21 17:32:03 -0400

    bug workaround: create your own toolchain

--- test/test.js
+++ test/test.js
@@ -6,24 +6,30 @@ let buildout = require("../out/combined.json");
 let types = buildout.contracts;
 let Balancer = types["src/Balancer.sol:Balancer"];
 let BalanceMath = types["src/BalanceMath.sol:BalanceMath"];
- 
-describe("BalanceMath", () => {
 
-    var web3 = new Web3(ganache.provider());
-    let BM = web3.eth.Contract(JSON.parse(BalanceMath.abi));
-   
-    BM.deploy({data: BalanceMath.bin}).send({from: "0x0"})
-    .then((bm) => {;
-        describe("add", () => {
-            it("should return the sum of two numbers", () => {
-                var c = bm.add(1, 2);
-            });
-            // etc
-            it("should not overflow", () => {
-            });
-        });
 
-        describe("mul", () => {
-        });
-    });
+var web3 = new Web3(ganache.provider());
+
+// web3.js / ganache-core bug, hangs on .send().then()
+// Can be extracted manually
+function deploy(type, cb) {
+    //console.log(type);
+    web3.eth.getAccounts().then((accounts) => {
+        let account = accounts[0];
+        web3.eth.Contract(JSON.parse(type.abi))
+            .deploy({data: type.bin})
+            .send({from: account, gas: 6000000}, (err,tx) => {
+   //             console.log(err, tx);
+                setTimeout(() => {web3.eth.getTransactionReceipt(tx, (err, receipt) => {
+                    //console.log(err, receipt);
+                    cb(receipt.contractAddress);
+                })}, 100);
+            })
+    })
+}
+
+deploy(BalanceMath, async (address) => {
+    var bm = web3.eth.Contract(JSON.parse(BalanceMath.abi), address);
+    var res = await bm.methods.add(1, 2).call();
+    console.log(res);
 });
