b5185bd6b3efe87b88c318ce20327bcb3642e335
Private Development Server In Bathroom <H@state.gov>
2019-07-24 00:15:08 +0000

    Refactoring BalanceMath and adding exact calculations formulae in pure js for testing routine

--- src/BalanceMath.sol
+++ src/BalanceMath.sol
@@ -13,4 +13,84 @@ contract BalanceMath is DSMath {
         uint256 wadExample = add(Qi, Qj);
         return add(Qi, Qj);
     }
+    
+    function approximateAmountOutForSell(uint256 QOut, uint256 QIn, uint256 qIn, uint256 wIn, uint256 wOut) public pure returns (uint256) {
+        if (wIn>wOut)
+        // Expand power into two, first with integer exponent >=1 and second with exponent <1
+        {
+            uint256 precision = uint256(10) ** 18; // TODO Use norm_factor from Balancer instead of precision
+            uint256 integerPower = power(precision,QIn,QIn+qIn,wIn/wOut);
+            return QOut - (integerPower * binExpqOut(QOut, QIn, qIn, wIn%wOut, wOut)/precision);
+        }
+        // Use binomial expansion directly since exponent <1
+        else{
+            return QOut-binExpqOut(QOut, QIn, qIn, wIn, wOut);
+        }
+
+    }
+
+     /**
+     * @notice This function
+     */
+    function binExpqOut(uint256 QOut, uint256 QIn, uint256 qIn, uint256 wIn, uint256 wOut) public pure returns (uint256) {
+        return QOut
+            -BinExpqOutTermN(QOut, QIn, qIn, wIn, wOut, 1)
+            -BinExpqOutTermN(QOut, QIn, qIn, wIn, wOut, 2)
+            -BinExpqOutTermN(QOut, QIn, qIn, wIn, wOut, 3)
+            -BinExpqOutTermN(QOut, QIn, qIn, wIn, wOut, 4)
+            -BinExpqOutTermN(QOut, QIn, qIn, wIn, wOut, 5);
+    }
+
+    function BinExpqOutTermN(uint256 QOut, uint256 QIn, uint256 qIn, uint256 wIn, uint256 wOut, uint256 n) public pure returns (uint256) {
+        if(n == 0){
+            return QOut;
+        }else if(n == 1)
+        {
+            return (((QOut*wIn)/wOut)*qIn)/(QIn+qIn)
+        }
+        else{
+            return ((((BinExpqOutTermN(QOut, QIn, qIn, wIn, wOut, n-1)*
+                ((n-1)*wOut-wIn))/wOut)*qIn)/n)/(QIn+qIn);
+        }
+    }
+
+    function approximateAmountInForBuy(uint256 QOut, uint256 QIn, uint256 qOut, uint256 wIn, uint256 wOut) public pure returns (uint256) {
+        if (wOut>wIn)
+        // Expand power into two, first with integer exponent >=1 and second with exponent <1
+        {
+            uint256 precision = uint256(10) ** 18; // TODO Use norm_factor from Balancer instead of precision
+            uint256 integerPower = power(precision,QOut,QOut-qOut,wOut/wIn);
+            return (integerPower * binExpqIn(QOut, QIn, qOut, wIn, wOut%wIn))/precision - QIn;
+        }
+        // Use binomial expansion directly since exponent <1
+        else{
+            return binExpqIn(QOut, QIn, qOut, wIn, wOut)-QIn;
+        }
+
+    }
+
+    /**
+     * @notice This function
+     */
+    function binExpqIn(uint256 QOut, uint256 QIn, uint256 qOut, uint256 wIn, uint256 wOut) public pure returns (uint256) {
+        return QIn
+            +binExpqInTermN(QOut, QIn, qOut, wIn, wOut, 1)
+            -binExpqInTermN(QOut, QIn, qOut, wIn, wOut, 2)
+            +binExpqInTermN(QOut, QIn, qOut, wIn, wOut, 3)
+            -binExpqInTermN(QOut, QIn, qOut, wIn, wOut, 4)
+            +binExpqInTermN(QOut, QIn, qOut, wIn, wOut, 5);
+    }
+
+    function binExpqInTermN(uint256 QOut, uint256 QIn, uint256 qOut, uint256 wIn, uint256 wOut, uint256 n) public pure returns (uint256) {
+        if(n == 0){
+            return QIn;
+        }else if(n == 1)
+        {
+            return (((QIn*wOut)/wIn)*qOut)/(QOut-qOut);
+        }
+        else{
+            return ((((binExpqInTermN(QOut, QIn, qIn, wIn, wOut, n-1)*
+                ((n-1)*wIn-wOut))/wIn)*qOut)/n)/(QOut-qOut);
+        }
+    }
 }
--- test/math.js
+++ test/math.js
@@ -1,14 +1,40 @@
 assert = require("assert");
 
-function binomExpansion() {
-    return 1;
+// Description: get qOut which is the amount of tokenOut a user gets when selling qIn tokenIn
+// QOut = tokenOut Balance in pool
+// QIn = tokenIn Balance in pool
+// qIn = amount of tokenIn being sold
+// wIn = tokenIn weight in pool
+// wOut = tokenOut Balance of pool
+// fee = pool fee
+function getAmountOutForSell(QOut, QIn, qIn, wIn, wOut, fee){
+    return (1-(QIn/(QIn+qIn*(1-fee)))**(wIn/wOut))*QOut;
 }
 
+// Description: get qIn which is the amount of tokenIn a user gets when buying qOut tokenOut
+// QOut = tokenOut Balance in pool
+// QIn = tokenIn Balance in pool
+// qOut = amount of tokenOut being bought
+// wIn = tokenIn weight in pool
+// wOut = tokenOut Balance of pool
+// fee = pool fee
+function getAmountInForBuy(QOut, QIn, qOut, wIn, wOut, fee){
+    return (((QOut/(QOut-qOut))**(wOut/wIn)-1)*QIn)/(1-fee)
+}
+
+QOut = 1000 * 10**18;
+QIn = 1000 * 10**18;
+qOut = 100 * 10**18;
+qIn =  100 * 10**18;
+wIn = 1000;
+wOut = 1000;
+fee = 0.001; // 1%
+
 describe("plain js math", () => {
-    it("binomial expansion", () => {
-        assert.equal(1, binomExpansion());
+    it("amountOut for sell should be", () => {
+        assert.equal(1, getAmountOutForSell(QOut, QIn, qIn, wIn, wOut, fee));
     });
-    it("buy amount formula", () => {
-        assert.equal(2, binomExpansion());
+    it("amountIn for buy should be", () => {
+        assert.equal(1, getAmountInForBuy(QOut, QIn, qOut, wIn, wOut, fee));
     });
 });
