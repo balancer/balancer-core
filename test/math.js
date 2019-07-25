cbcaf356e8c035190bdddd251bed45f3ca91661d
Private Development Server In Bathroom <H@state.gov>
2019-07-25 17:26:26 +0000

    Revised 4 new test cases with different weight ratios (2, 3, 10 and 100)

--- test/math.js
+++ test/math.js
@@ -7,9 +7,14 @@ describe("Checking exact math in plain js", () => {
         assert.equal(10, bMath.swapSpecifyInMath(20, 20, 20, 10, 10, 0)); // Weight ratio 1
 
         assert.equal(15, bMath.swapSpecifyInMath(20, 20, 20, 2, 1, 0)); // Weight ratio 2
-        assert.equal(16, bMath.swapSpecifyInMath(16, 16, 16, 3, 1, 0)); // Weight ratio 3
-        assert.equal(270, bMath.swapSpecifyInMath(1000, 500, 16, 10, 1, 0)); // Weight ratio 10
-        assert.equal(10, bMath.swapSpecifyInMath(2000, 1000, 50, 100, 1, 0)); // MAX Weight ratio 100
+        assert.equal(14, bMath.swapSpecifyInMath(16, 16, 16, 3, 1, 0)); // Weight ratio 3
+        assert.equal(1031, bMath.swapSpecifyInMath(1032, 500, 500, 10, 1, 0)); // Weight ratio 10
+        assert.equal(1267650600228229401496703205376-1,
+            bMath.swapSpecifyInMath(1267650600228229401496703205376, 10, 10, 100, 1, 0)); // MAX Weight ratio 100
+
+        //assert.equal(10, bMath.swapSpecifyInMath(2000, 1000, 50, 1, 2, 0)); // Weight ratio 1/2
+        //assert.equal(10, bMath.swapSpecifyInMath(800, 100, 50, 1, 3, 0)); // Weight ratio 1/3
+        //assert.equal(10, bMath.swapSpecifyInMath(800, 100, 50, 1, 3, 0)); // Weight ratio 1/10
     });
     it("Exact values for some approximation points", () => {
         assert.equal(1, bMath.swapSpecifyInMath_Approx(2, 2, 2, 1, 1, 0));
