99c96e33b126921e2c8324e312defbd3c4d891b8
Nikolai Mushegian <hi@nikolai.fyi>
2019-07-25 14:09:49 -0400

    balancer

--- src/Balancer.sol
+++ src/Balancer.sol
@@ -4,18 +4,46 @@ import 'erc20/erc20.sol';
 import 'ds-note/note.sol';
 import 'ds-token/token.sol';
 
-contract Balancer is DSMath {
+import "./BalanceMath.sol";
+
+contract Balancer is BalanceMath {
     address manager;
+    uint256 feeRatio;
+
+    ERC20 tokA;
+    uint256 weightA;
+    uint256 balanceA;
+
+    ERC20 tokB;
+    uint256 weightB;
+    uint256 balanceB;
+
     constructor() public {
         manager = msg.sender;
     }
 
-    function swapInExact(uint256 amountIn, address i, address o)
-        public returns (uint256 amountOut)
+    function swapInExact(uint256 amountIn, ERC20 i, ERC20 o)
+        public returns (uint256 amountOut, uint256 feeAmount)
     {
-        amountOut = 1*WAD;
-        DSToken(i).pull(msg.sender, amountIn);
-        DSToken(o).push(msg.sender, amountOut);
-        return amountOut;
+        ERC20 tin; ERC20 tout;
+        uint256 tinWeight; uint256 tinBalance;
+        uint256 toutWeight; uint256 toutBalance;
+
+        if (i == tokA && o == tokB) {
+            tin = tokA; tout = tokB;
+            tinWeight = weightA; toutWeight = weightB;
+            tinBalance = balanceA; toutBalance = balanceB;
+        } else if (i == tokB && o == tokA) {
+            revert();
+        } else {
+            revert();
+        }
+
+        (amountOut, feeAmount) = swapInExactMath( tinBalance, tinWeight
+                                                , toutBalance, toutWeight
+                                                , amountIn, feeRatio );
+        i.transferFrom(msg.sender, address(this), amountIn);
+        o.transfer(msg.sender, amountOut);
+        return (amountOut, feeAmount);
     }
 }
