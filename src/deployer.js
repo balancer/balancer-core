f26f5cb761156cf639f457c64cdf28e7b9c7d1cc
admin <admin@computer.local>
2019-08-10 16:27:51 -0400

    refactor

--- src/deployer.js
+++ src/deployer.js
@@ -1,21 +1,32 @@
-module.exports.deployType = async function (web3, type, cb) {
+module.exports.deployTestEnv = async function(web3, buildout) {
+    var objects = { // Base scenario universe
+        acct0: undefined,
+        math: undefined,
+        bTest: undefined,
+    };
+    let types = buildout.contracts;
+    let Balancer = types["src/Balancer.sol:Balancer"];
+    let BalanceMath = types["src/BalanceMath.sol:BalanceMath"];
+    let BalanceTest = types["src/BalanceTest.sol:BalanceTest"];
+
+    objects.accts = await web3.eth.getAccounts();
+    objects.acct0 = objects.accts[0];
+    objects.math = await module.exports.deployType(web3, BalanceMath);
+    objects.bTest = await module.exports.deployType(web3, BalanceTest);
+    //console.log(objects);
+    return objects;
+}
+
+module.exports.deployType = async function (web3, type) {
     //console.log(type);
     if(type.bin == '') {
         throw new Error("Trying to deploy contract with empty `bin`");
     }
     let accounts = await web3.eth.getAccounts();
     acct0 = accounts[0];
-    new web3.eth.Contract(JSON.parse(type.abi))
-        .deploy({data: type.bin})
-        .send({from: acct0, gas: 6000000}, (err,tx) => {
-            //console.log(err, tx);
-            setTimeout(() => {
-                web3.eth.getTransactionReceipt(tx, (err, receipt) => {
-                    //console.log(err, receipt);
-                    cb(receipt.contractAddress);
-                })
-            }, 25);
-        })
+    return new web3.eth.Contract(JSON.parse(type.abi))
+            .deploy({data: type.bin})
+            .send({from: acct0, gas: 0xfffffff});
 }
 
 
--- test/contracts.js
+++ test/contracts.js
@@ -2,6 +2,8 @@ let assert = require("assert");
 let Web3 = require("web3");
 let ganache = require("ganache-core");
 
+let deployer = require("../src/deployer.js")
+
 let buildout = require("../out/combined.json");
 let types = buildout.contracts;
 let Balancer = types["src/Balancer.sol:Balancer"];
@@ -17,41 +19,14 @@ let RAY = web3.utils.toBN('1000000000000000000000000000');
 let WAD = web3.utils.toBN('1000000000000000000');
 let bn = (num) => { return web3.utils.toBN(num); }
 
-
 var objects = { // Base scenario universe
     acct0: undefined,
     math: undefined,
     bTest: undefined,
 };
 
-// TODO untangle spaghetti
-beforeEach((done) => {
-    // web3.js / ganache-core bug, hangs on .send().then()
-    // Can be extracted manually
-    async function deploy(type, cb) {
-        //console.log(type);
-        if(type.bin == '') {
-            throw new Error("Trying to deploy contract with empty `bin`");
-        }
-        let accounts = await web3.eth.getAccounts();
-        objects.acct0 = accounts[0];
-        new web3.eth.Contract(JSON.parse(type.abi))
-            .deploy({data: type.bin})
-            .send({from: objects.acct0, gas: 6000000}, (err,tx) => {
-                web3.eth.getTransactionReceipt(tx, (err, receipt) => {
-                    //console.log(err, receipt);
-                    cb(receipt.contractAddress);
-                })
-            })
-    }
-
-    deploy(BalanceMath, (address) => {
-        objects.math = new web3.eth.Contract(JSON.parse(BalanceMath.abi), address);
-        deploy(BalanceTest, (address) => {
-            objects.bTest = new web3.eth.Contract(JSON.parse(BalanceTest.abi), address);
-            done();
-        });
-    });
+beforeEach(async () => {
+    objects = await deployer.deployTestEnv(web3, buildout);
 });
 
 describe("balanceMath", function() {
