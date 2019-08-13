1098ac137177548152625e27a885ca8e1e11bd46
Nikolai Mushegian <hi@nikolai.fyi>
2019-08-13 14:57:34 -0400

    setParams

--- pkg.js
+++ pkg.js
@@ -3,15 +3,21 @@ let buildout = require("./evm/combined.json");
 let types = buildout.contracts;
 
 function lift(type) {
-    console.log(`lifting ${type}`);
     types[type] = types[`src/${type}.sol:${type}`];
     types[`src/${type}.sol:${type}`] = undefined;
 }
 
+lift("BalancerMath");
+lift("BalancerPool");
+lift("BToken");
+
 module.exports.deploy = async function(web3, from, typeName, args) {
     let type = types[typeName];
+    if(type == undefined) {
+        throw new Error(`Trying to deploy ${typeName}, but type name not recognized`);
+    }
     if(type.bin == '') {
-        throw new Error("Trying to deploy contract with empty `bin`");
+        throw new Error(`Trying to deploy contract ${typeName} with empty \`bin\``);
     }
     return new web3.eth.Contract(JSON.parse(type.abi))
             .deploy({data: type.bin, arguments: args})
