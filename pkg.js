4de267de937c64022316dcc96fcae4ef2106f4a0
nikolai <mail@nikolai.fyi>
2019-08-14 02:47:23 -0400

    refactor pkg.types->util/types

--- pkg.js
+++ pkg.js
@@ -1,26 +1,3 @@
 module.exports.floatMath = require("./util/floatMath.js");
-
-let buildout = require("./evm/combined.json");
-let types = buildout.contracts;
-function lift(type) {
-    types[type] = types[`sol/${type}.sol:${type}`];
-    types[`sol/${type}.sol:${type}`] = undefined;
-}
-lift("BalancerMath");
-lift("BalancerPool");
-lift("BToken");
-module.exports.types = types;
-
-module.exports.deploy = async function(web3, from, typeName, args) {
-    let type = types[typeName];
-    if(type == undefined) {
-        throw new Error(`Trying to deploy ${typeName}, but type name not recognized`);
-    }
-    if(type.bin == '') {
-        throw new Error(`Trying to deploy contract ${typeName} with empty \`bin\``);
-    }
-    return new web3.eth.Contract(JSON.parse(type.abi))
-            .deploy({data: type.bin, arguments: args})
-            .send({from: from, gas: 0xfffffff});
-}
-
+module.exports.deploy = require("./util/types.js").deploy;
+module.exports.types = require("./util/types.js").types;
