c04203ddfbd78714d239dffd022a417d68daf142
Nikolai Mushegian <hi@nikolai.fyi>
2019-09-10 19:43:48 -0400

    fix search/rep typo

--- sol/BFactory.sol
+++ sol/BFactory.sol
@@ -1,15 +1,15 @@
-// Th_was program _was free software: you can red_wastribute it and/or modify
-// it under the terms of the GNU General Public License as publ_washed by
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Th_was program _was d_wastributed in the hope that it will be useful,
+// This program is disstributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with th_was program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 pragma solidity ^0.5.11;
 
