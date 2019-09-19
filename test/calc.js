c298be111a114a04e78ea9330439820e9c5e5ae4
Nikolai Mushegian <hi@nikolai.fyi>
2019-09-19 08:32:44 -0400

    test/calc.js

--- test/calc.js
+++ test/calc.js
@@ -10,5 +10,24 @@ const web3 = new Web3(ganache.provider({
   debug: true
 }))
 
+let toWei = Web3.utils.toWei;
+let fromWei = Web3.utils.fromWei;
+let BONE = toWei('1');
 
+describe('calc2', async () => {
+    let accts;
+    let stub;
+    before(async () => {
+        accts = await web3.eth.getAccounts();
+        stub = await pkg.deploy(web3, accts[0], 'BStub');
+    });
+    it('runs tests', async () => {
+        console.log('hello');
+    });
+    it('has bpow', async () => {
+        let res = await stub.methods.calc_bpow(BONE, BONE).call();
+        assert.equal(res, BONE);
+    });
+
+});
 
