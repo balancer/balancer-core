e8e99369c66022b089b8c01e1711328edc366faa
Nikolai Mushegian <hi@nikolai.fyi>
2019-10-14 19:54:03 -0400

    lifecycle test stubs

--- test/lifecycle.js
+++ test/lifecycle.js
@@ -56,4 +56,67 @@ contract('BPool lifecycle', async (accounts) => {
     assert(isBound);
     assert.equal(1, numTokens);
   });
+
+  it('newBPool', async () => {
+      throw 'unimplemented';
+  });
+
+  it('bind getNumTokens isBound', async () => {
+      throw 'unimplemented';
+  });
+
+  it('bind ERR_NOT_CONTROLLER ERR_IS_BOUND ERR_IS_FINALIZED', async () => {
+      throw 'unimplemented';
+  });
+
+  it('bind ERR_MAX_TOKENS', async () => {
+      throw 'unimplemented';
+  });
+
+  it('unbind getNumTokens isBound', async()=>{
+      throw 'unimplemented';
+  });
+
+  it('unbind ERR_NOT_CONTROLLER ERR_NOT_BOUND ERR_IS_FUNDED', async () => {
+      throw 'unimplemented';
+  });
+
+  it('clear getBalance getWeight getNumTokens isBound isFunded', async () => {
+      throw 'unimplemented';
+  });
+
+  it('clear ERR_NOT_CONTROLLER ERR_IS_FINALIZED', async () => {
+      throw 'unimplemented';
+  });
+
+  it('setParams getBalance getWeight getSpotPrice', async () => {
+      throw 'unimplemented';
+  });
+
+  it('setParams ERR_NOT_CONTROLLER ERR_NOT_BOUND ERR_IS_FINALIZED', async () => {
+      throw 'unimplemented';
+  });
+
+  it('setParams ERR_MIN_WEIGHT ERR_MAX_WEIGHT ERR_MIN_BALANCE ERR_MAX_BALANCE', async () => {
+      throw 'unimplemented';
+  });
+
+  it('setParams ERR_MAX_TOTAL_WEIGHT', async () => {
+      throw 'unimplemented';
+  });
+
+  it('setParams ERR_BTOKEN_UNDERFLOW ERR_ERC20_FALSE', async () => {
+      throw 'unimplemented';
+  });
+
+  it('clear ERR_NOT_CONTROLLER ERR_NOT_FINALIZED getBalance getTokens', async () => {
+  });
+
+  it('setFees ERR_NOT_CONTROLLER ERR_MAX_FEE getFees', async () => {
+      throw 'unimplemented';
+  });
+
+  it('setController getController ERR_NOT_CONTROLLER', async () => {
+      throw 'unimplemented'
+  });
 });
