048d96ef66b39b4cb62f0d9c0694fd537d17122c
Mike McDonald <mikeraymcdonald@gmail.com>
2019-11-28 12:40:52 -0500

    eslint airbnb style for all tests

--- test/math_no_fees.js
+++ test/math_no_fees.js
@@ -2,126 +2,123 @@ const BPool = artifacts.require('BPool');
 const BFactory = artifacts.require('BFactory');
 const TToken = artifacts.require('TToken');
 const TTokenFactory = artifacts.require('TTokenFactory');
-const errorDelta = 10**-8;
+const errorDelta = 10 ** -8;
 const verbose = process.env.VERBOSE;
 
 function calcRelativeDiff(_expected, _actual) {
-  return Math.abs((_expected - _actual)/_expected);
+    return Math.abs((_expected - _actual) / _expected);
 }
 
-contract('math tests from canonical setup', async (accounts) => {
-  const admin = accounts[0];
-  const toHex = web3.utils.toHex;
-  const toWei = web3.utils.toWei;
-  const fromWei = web3.utils.fromWei;
+contract('math tests from canonical setup', async () => {
+    const { toHex } = web3.utils;
+    const { toWei } = web3.utils;
 
-  const MAX = web3.utils.toTwosComplement(-1);
+    const MAX = web3.utils.toTwosComplement(-1);
 
-  let tokens;           // token factory / registry
-  let DIRT, ROCK, SAND; // addresses
-  let dirt, rock, sand; // TTokens
-  let factory;          // BPool factory
-  let pool;             // first pool w/ defaults
-  let POOL;             //   pool address
+    let tokens; // token factory / registry
+    let DIRT; let ROCK; let
+        SAND; // addresses
+    let dirt; let rock; let
+        sand; // TTokens
+    let factory; // BPool factory
+    let pool; // first pool w/ defaults
+    let POOL; //   pool address
 
-  const dirtBalance = toWei('6')
-  const dirtDenorm = toWei('10');
+    const dirtBalance = toWei('6');
+    const dirtDenorm = toWei('10');
 
-  const rockBalance = toWei('8')
-  const rockDenorm = toWei('10');
+    const rockBalance = toWei('8');
+    const rockDenorm = toWei('10');
 
-  const sandBalance = toWei('24')
-  const sandDenorm = toWei('20');
+    const sandBalance = toWei('24');
+    const sandDenorm = toWei('20');
 
-  const sumWeigths = parseInt(dirtDenorm)+parseInt(rockDenorm);
-  const dirtNorm = parseInt(dirtDenorm)/sumWeigths;
-  const rockNorm = parseInt(rockDenorm)/sumWeigths;
+    const sumWeights = parseInt(dirtDenorm, 10) + parseInt(rockDenorm, 10);
+    const dirtNorm = parseInt(dirtDenorm, 10) / sumWeights;
+    const rockNorm = parseInt(rockDenorm, 10) / sumWeights;
 
-  before(async () => {
-    tokens = await TTokenFactory.deployed();
-    factory = await BFactory.deployed();
+    before(async () => {
+        tokens = await TTokenFactory.deployed();
+        factory = await BFactory.deployed();
 
-    POOL = await factory.newBPool.call(); // this works fine in clean room
-    await factory.newBPool();
-    pool = await BPool.at(POOL);
-
-
-    await tokens.build(toHex("DIRT"));
-    await tokens.build(toHex("ROCK"));
-    await tokens.build(toHex("SAND"));
-
-    DIRT = await tokens.get.call(toHex("DIRT"));
-    ROCK = await tokens.get.call(toHex("ROCK"));
-    SAND = await tokens.get.call(toHex("SAND"));
-
-    dirt = await TToken.at(DIRT);
-    rock = await TToken.at(ROCK);
-    sand = await TToken.at(SAND);
-
-    await dirt.mint(MAX);
-    await rock.mint(MAX);
-    await sand.mint(MAX);
-
-    await dirt.approve(POOL, MAX);
-    await rock.approve(POOL, MAX);
-    await sand.approve(POOL, MAX);
+        POOL = await factory.newBPool.call(); // this works fine in clean room
+        await factory.newBPool();
+        pool = await BPool.at(POOL);
 
 
-    await pool.bind(DIRT, dirtBalance, dirtDenorm);
-    await pool.bind(ROCK, rockBalance, rockDenorm);
-    await pool.bind(SAND, sandBalance, sandDenorm);
+        await tokens.build(toHex('DIRT'));
+        await tokens.build(toHex('ROCK'));
+        await tokens.build(toHex('SAND'));
 
-    await pool.setPublicSwap(true);
-    
-  });
+        DIRT = await tokens.get.call(toHex('DIRT'));
+        ROCK = await tokens.get.call(toHex('ROCK'));
+        SAND = await tokens.get.call(toHex('SAND'));
 
-  it('swapExactMarginalPrice', async () => {
-    let MarPrice = 1;
+        dirt = await TToken.at(DIRT);
+        rock = await TToken.at(ROCK);
+        sand = await TToken.at(SAND);
 
-    let output = await pool.swapExactMarginalPrice.call(DIRT, MAX, ROCK, '0', toWei(String(MarPrice)));
-    let Ai = parseInt(output[0]);
-    let Ao = parseInt(output[1]);
+        await dirt.mint(MAX);
+        await rock.mint(MAX);
+        await sand.mint(MAX);
 
-    // Checking outputs
-        expected = toWei(String(48**0.5-6));
-    let actual = Ai;
-    let relDif = calcRelativeDiff(expected,actual);
-    if(verbose){
-        console.log(`Ai`);
-        console.log(`expected: ${expected})`);
-        console.log(`actual  : ${actual})`);
-        console.log(`relDif  : ${relDif})`);
-    }
-  
-    assert.isAtMost(relDif, errorDelta);
+        await dirt.approve(POOL, MAX);
+        await rock.approve(POOL, MAX);
+        await sand.approve(POOL, MAX);
 
 
-    expected = toWei(String(8-48**0.5))
-    actual = Ao;
-    relDif = calcRelativeDiff(expected,actual);
-    if(verbose){
-        console.log(`Ao`);
-        console.log(`expected: ${expected})`);
-        console.log(`actual  : ${actual})`);
-        console.log(`relDif  : ${relDif})`);
-    } 
-    assert.isAtMost(relDif, errorDelta);  
+        await pool.bind(DIRT, dirtBalance, dirtDenorm);
+        await pool.bind(ROCK, rockBalance, rockDenorm);
+        await pool.bind(SAND, sandBalance, sandDenorm);
 
-    // Checking outputs
-    expected = MarPrice;
-    actual = (parseInt(dirtBalance)+Ai)*rockNorm/((parseInt(rockBalance)-Ao)*dirtNorm);
-    relDif = calcRelativeDiff(expected,actual);
-    if(verbose){
-        console.log(`Ai: ${Ai})`);
-        console.log(`Ao: ${Ao})`);
-        console.log(`MarPrice`);
-        console.log(`expected: ${expected})`);
-        console.log(`actual  : ${actual})`);
-        console.log(`relDif  : ${relDif})`);
-    }
-  
-    assert.isAtMost(relDif, errorDelta);
-  });
+        await pool.setPublicSwap(true);
+    });
+
+    it('swapExactMarginalPrice', async () => {
+        const MarPrice = 1;
+
+        const output = await pool.swapExactMarginalPrice.call(DIRT, MAX, ROCK, '0', toWei(String(MarPrice)));
+        const Ai = parseInt(output[0], 10);
+        const Ao = parseInt(output[1], 10);
+
+        // Checking outputs
+        let expected = toWei(String(48 ** 0.5 - 6));
+        let actual = Ai;
+        let relDif = calcRelativeDiff(expected, actual);
+        if (verbose) {
+            console.log('Ai');
+            console.log(`expected: ${expected})`);
+            console.log(`actual  : ${actual})`);
+            console.log(`relDif  : ${relDif})`);
+        }
+
+        assert.isAtMost(relDif, errorDelta);
 
 
+        expected = toWei(String(8 - 48 ** 0.5));
+        actual = Ao;
+        relDif = calcRelativeDiff(expected, actual);
+        if (verbose) {
+            console.log('Ao');
+            console.log(`expected: ${expected})`);
+            console.log(`actual  : ${actual})`);
+            console.log(`relDif  : ${relDif})`);
+        }
+        assert.isAtMost(relDif, errorDelta);
+
+        // Checking outputs
+        expected = MarPrice;
+        actual = ((parseInt(dirtBalance, 10) + Ai) * rockNorm) / ((parseInt(rockBalance, 10) - Ao) * dirtNorm);
+        relDif = calcRelativeDiff(expected, actual);
+        if (verbose) {
+            console.log(`Ai: ${Ai})`);
+            console.log(`Ao: ${Ao})`);
+            console.log('MarPrice');
+            console.log(`expected: ${expected})`);
+            console.log(`actual  : ${actual})`);
+            console.log(`relDif  : ${relDif})`);
+        }
+
+        assert.isAtMost(relDif, errorDelta);
+    });
 });
--- test/num.js
+++ test/num.js
@@ -1,37 +1,35 @@
 const truffleAssert = require('truffle-assertions');
+
 const TMath = artifacts.require('TMath');
 
-contract('TMath', async (accounts) => {
+contract('TMath', async () => {
+    const MAX = web3.utils.toTwosComplement(-1);
 
-  const MAX = web3.utils.toTwosComplement(-1);
+    describe('BMath', () => {
+        let tmath;
+        before(async () => {
+            tmath = await TMath.deployed();
+        });
 
-  describe('BMath', () => {
-    let tmath;
-    before(async () => {
-      tmath = await TMath.deployed();
+        it('badd throws on overflow', async () => {
+            await truffleAssert.reverts(tmath.calc_badd(1, MAX), 'ERR_ADD_OVERFLOW');
+        });
+
+        it('bsub throws on underflow', async () => {
+            await truffleAssert.reverts(tmath.calc_bsub(1, 2), 'ERR_SUB_UNDERFLOW');
+        });
+
+        it('bmul throws on overflow', async () => {
+            await truffleAssert.reverts(tmath.calc_bmul(2, MAX), 'ERR_MUL_OVERFLOW');
+        });
+
+        it('bdiv throws on div by 0', async () => {
+            await truffleAssert.reverts(tmath.calc_bdiv(1, 0), 'ERR_DIV_ZERO');
+        });
+
+        it('bpow throws on base outside range', async () => {
+            await truffleAssert.reverts(tmath.calc_bpow(0, 2), 'ERR_BPOW_BASE_TOO_LOW');
+            await truffleAssert.reverts(tmath.calc_bpow(MAX, 2), 'ERR_BPOW_BASE_TOO_HIGH');
+        });
     });
-
-    it('badd throws on overflow', async () => {
-      await truffleAssert.reverts(tmath.calc_badd(1, MAX), 'ERR_ADD_OVERFLOW');
-    });
-
-    it('bsub throws on underflow', async () => {
-      await truffleAssert.reverts(tmath.calc_bsub(1, 2), 'ERR_SUB_UNDERFLOW');
-    });
-
-    it('bmul throws on overflow', async () => {
-      await truffleAssert.reverts(tmath.calc_bmul(2, MAX), 'ERR_MUL_OVERFLOW');
-    });
-
-    it('bdiv throws on div by 0', async () => {
-      await truffleAssert.reverts(tmath.calc_bdiv(1, 0), 'ERR_DIV_ZERO');
-    });
-
-    it('bpow throws on base outside range', async () => {
-      await truffleAssert.reverts(tmath.calc_bpow(0, 2), 'ERR_BPOW_BASE_TOO_LOW');
-      await truffleAssert.reverts(tmath.calc_bpow(MAX, 2), 'ERR_BPOW_BASE_TOO_HIGH');
-    });
-
-  });
-
 });
