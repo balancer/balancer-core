25073aa245a1b3ba27561b2a756f1c4ca3c2f5c9
Illia Polosukhin <ilblackdragon@gmail.com>
2020-08-17 04:04:21 -0700

    Adding NEAR for local testing

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,37 @@
+const web3 = require('web3');
+const { NearProvider, nearlib, utils } = require('near-web3-provider');
+
+const NEAR_LOCAL_NETWORK_ID = 'local';
+const NEAR_LOCAL_URL = 'http://127.0.0.1:3030';
+
+const keyStore = new nearlib.keyStores.UnencryptedFileSystemKeyStore('neardev');
+const NEAR_LOCAL_ACCOUNT_ID = 'test.near';
+const NEAR_LOCAL_EVM = 'evm.test.near';
+const NEAR_LOCAL_EVM_ACCOUNT_ID = utils.nearAccountToEvmAddress(NEAR_LOCAL_ACCOUNT_ID);
+const LOTS_OF_GAS = "0xffffffffffffffffff";
+
 module.exports = {
     networks: {
         development: {
-            host: 'localhost', // Localhost (default: none)
-            port: 8545, // Standard Ethereum port (default: none)
-            network_id: '*', // Any network (default: none)
-            gas: 10000000,
+            network_id: "*",
+            skipDryRun: true,
+            from: NEAR_LOCAL_EVM_ACCOUNT_ID,
+            gas: LOTS_OF_GAS,
+            provider: () => {
+                const provider = new NearProvider(
+                    NEAR_LOCAL_URL, keyStore, NEAR_LOCAL_ACCOUNT_ID,
+                    NEAR_LOCAL_NETWORK_ID, NEAR_LOCAL_EVM,
+                );
+                utils.createTestAccounts(provider, 5);
+                return provider;
+            },
         },
+        // development: {
+        //     host: 'localhost', // Localhost (default: none)
+        //     port: 8545, // Standard Ethereum port (default: none)
+        //     network_id: '*', // Any network (default: none)
+        //     gas: 10000000,
+        // },
         coverage: {
             host: 'localhost',
             network_id: '*',
